e35860ba2d6c72388fef592ca05f553e498c8c0c
NI57721 <NI57721@users.noreply.github.com>
2025-06-24 17:01:56 +0900

    Set the quality value for converting AVIFs

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,7 +5,7 @@ shopt -s globstar nullglob
 for file in upload/**/*; do
   case "$file" in
     *.jpg | *.jpeg | *.png | *.gif | *.avif)
-      magick $file -resize '820>' ${file%.*}.avif.converted.avif
+      magick $file -quality 60% -resize '820>' ${file%.*}.avif.converted.avif
       mv ${file%.*}.avif.converted{.avif,}
       ;;
   esac

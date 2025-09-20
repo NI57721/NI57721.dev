25195775a2677aa660eca7f6eb8d88afb41e5a34
NI57721 <NI57721@users.noreply.github.com>
2025-09-20 20:19:43 +0900

    Add astro/react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 import icon from "astro-icon";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://NI57721.dev',
@@ -25,5 +27,5 @@ export default defineConfig({
     },
   },
 
-  integrations: [icon()],
+  integrations: [icon(), react()],
 });

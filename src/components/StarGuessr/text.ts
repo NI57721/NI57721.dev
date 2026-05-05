50692e37dc58836e1182c2f7429ab13ee0ccc9c6
NI57721 <NI57721@users.noreply.github.com>
2026-05-06 06:18:12 +0900

    Refactor

--- src/components/StarGuessr/text.ts
+++ src/components/StarGuessr/text.ts
@@ -1,20 +1,21 @@
-import type { Lang } from './StarGuessrApp';
+import type { Lang } from "./";
 
 export const text = {
   en: {
-    title: 'StarGuessr',
-    description: 'Guess the position of stars and deep-sky objects on the celestial sphere.',
-    noHint: 'No Hint',
-    hint: 'Hint',
-    start: 'Start',
-    backToMenu: 'Back to Menu',
+    title: "StarGuessr",
+    description:
+      "Guess the position of stars and deep-sky objects on the celestial sphere.",
+    noHint: "No Hint",
+    hint: "Hint",
+    start: "Start",
+    backToMenu: "Back to Menu",
   },
   ja: {
-    title: 'StarGuessr',
-    description: '星や天体の位置を当てよう。',
-    noHint: 'ノーヒント',
-    hint: 'ヒントあり',
-    start: 'スタート',
-    backToMenu: 'メニューに戻る',
+    title: "StarGuessr",
+    description: "星や天体の位置を当てよう。",
+    noHint: "ノーヒント",
+    hint: "ヒントあり",
+    start: "スタート",
+    backToMenu: "メニューに戻る",
   },
 } satisfies Record<Lang, Record<string, string>>;

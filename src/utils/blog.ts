556e024c567b8ba34470c83c32fd47293cd9a724
NI57721 <NI57721@users.noreply.github.com>
2026-09-20 15:55:39 +0900

    Format Date
    WIP for date paths

---
+++ src/utils/blog.ts
@@ -0,0 +1,95 @@
+export type BlogLanguage = "en" | "ja";
+
+const monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export function formatBlogDate(
+  year: string,
+  month: string,
+  day: string,
+  lang: BlogLanguage,
+): string {
+  if (lang === "ja") {
+    return `${year}年${month}月${day}日`;
+  } else if (lang === "en") {
+    return `${day} ${monthNames[Number(month) - 1]} ${year}`;
+  } else {
+    return `${day} ${monthNames[Number(month) - 1]} ${year}`;
+  }
+}
+
+export function formatBlogMonth(
+  year: string,
+  month: string,
+  lang: BlogLanguage,
+): string {
+  if (lang === "ja") {
+    return `${year}年${month}月`;
+  } else if (lang === "en") {
+    return `${monthNames[Number(month) - 1]} ${year}`;
+  } else {
+    return `${monthNames[Number(month) - 1]} ${year}`;
+  }
+}
+
+export function formatBlogYear(year: string, lang: BlogLanguage): string {
+  if (lang === "ja") {
+    return `${year}年`;
+  } else if (lang === "en") {
+    return `${year}`;
+  } else {
+    return `${year}`;
+  }
+}
+
+export function formatDateObject(date: Date, lang: BlogLanguage): string {
+  if (lang === "ja") {
+    return date.toLocaleDateString("ja-JP", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+    });
+  } else if (lang === "en") {
+    return (
+      date.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }) +
+      ", " +
+      date.toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+    );
+  } else {
+    return (
+      date.toLocaleDateString("en-GB", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      }) +
+      ", " +
+      date.toLocaleTimeString("en-GB", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+      })
+    );
+  }
+}
